47215278e37b989c270b7cc16b7e5d6e0e87931e
dananjaya <dananjaya.g@msystechnologies.com>
2017-12-03 00:14:07 +0530

    Added

--- src/Util/db.js
+++ src/Util/db.js
@@ -2,10 +2,10 @@ var mysql = require('promise-mysql');
 var pool;
  
 pool = mysql.createPool({
-  host: "127.0.0.1",
-  user: "ragadmin",
-  password: "ragadmin123",
-  database: "dbsoundarya",
+  host: "148.66.137.19",
+  user: "db_soundaryainst",
+  password: "dbsoundadmin@123",
+  database: "db_soundaryainst",
   connectionLimit: 10,
   connectTimeout  : 60 * 60 * 1000,
   aquireTimeout   : 60 * 60 * 1000,
